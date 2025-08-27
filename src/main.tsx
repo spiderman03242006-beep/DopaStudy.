468abf2299d648c5ecbd69a3d61b9a24db50c9f8
Ono Yoshihiro <b25t2024y@edu.tottori-u.ac.jp>
2025-08-27 23:23:19 +0900

    fix: use default ChakraProvider theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { HashRouter } from "react-router-dom"
-import { ChakraProvider, extendTheme } from "@chakra-ui/react"
-
-// デフォルトテーマを生成
-const theme = extendTheme({})
+import { ChakraProvider } from "@chakra-ui/react"   // ← extendTheme は消す
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>   {/* ← theme を渡さなくてもOK（デフォルト適用） */}
       <HashRouter>
         <App />
       </HashRouter>
     </ChakraProvider>
   </React.StrictMode>,
 )
-
