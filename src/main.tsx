99817bb26cd7b12dd050aac6bcd5c06ecfb8a596
Ono Yoshihiro <b25t2024y@edu.tottori-u.ac.jp>
2025-08-27 23:39:38 +0900

    build success with chakra v3

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { HashRouter } from "react-router-dom"
-import { ChakraProvider } from "@chakra-ui/react"   // ← extendTheme は消す
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/theme-utils";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = extendTheme({});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>   {/* ← theme を渡さなくてもOK（デフォルト適用） */}
+    <ChakraProvider theme={theme}>
       <HashRouter>
         <App />
       </HashRouter>
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
